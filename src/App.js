f61b6222e75cbc4c5b4c9fac598d5c3185e1b5fb
James Burgess <jamesmillerburgess@gmail.com>
2017-09-07 15:43:22 +0200

    Work done with nesrin and micheline yesterday

--- src/App.js
+++ src/App.js
@@ -6,8 +6,40 @@ class App extends Component {
     return (
       <div className="App">
         <div className="container">
-          <div className="jumbotron">
-            <h1 className="display-3">Verified Gross Mass FPOC</h1>
+          <div className="form-body">
+            <div className="header-row">
+              <div>VGM Submission</div>
+              <div className="header-right">
+                <div>Not Started</div>
+                <button className="dismiss-button">X</button>
+              </div>
+            </div>
+
+            <div className="radio-control">
+              <input type="radio" />
+              I have submitted the verified gross weight to the carrier for the
+              below selected container(s).
+            </div>
+            <div className="container-list-item">
+              <input type="checkbox" />
+              MSCU 123 456 -9
+            </div>
+            <div className="container-list-item">
+              <input type="checkbox" />
+              MSCU 123 456 -8
+            </div>
+            <div className="container-list-item">
+              <input type="checkbox" />
+              MSCU 123 456 -7
+            </div>
+            <div className="container-list-item">
+              <input type="checkbox" />
+              MSCU 123 456 -0
+            </div>
+          </div>
+          <div className="footer-row">
+            <button>Complete</button>
+            <button>Cancel</button>
           </div>
         </div>
       </div>
